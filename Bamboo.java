a9bde340c6096f4b484878f762c01c40e1c7c147
lama nawaraj <lama.nawraj00@gmail.com>
2022-01-18 15:20:41 +0900

    calling bamboo class with example of polimorphism

--- Bamboo.java
+++ Bamboo.java
@@ -1,5 +1,35 @@
 package BasicjavaWithGit;
 
 public class Bamboo {
-    
+    public static void main(String[] args) {
+
+        Jungle bt = new BambooTree();
+        bt.showTree();
+    }
+}
+
+class BambooTree extends Tree {
+
+    public BambooTree() {
+        System.out.println("This is bamboo Tree");
+    }
+
+    @Override
+    public void showTree() {
+        System.out.println("Bamboo is special tree.");
+    }
+}
+
+class Tree implements Jungle {
+
+    @Override
+    public void showTree() {
+        System.out.println("This trees are from jungle");
+
+    }
+
+}
+
+interface Jungle {
+    void showTree();
 }
